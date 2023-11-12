d33d520f9c66d3571569182cb3b6144342acc196
kimiyashabani <shabani.kimiya@gmail.com>
2023-11-12 23:52:21 +0100

    getting current location update 2

--- app/src/main/java/com/firstapp/go2meet_map/MapsActivity.java
+++ app/src/main/java/com/firstapp/go2meet_map/MapsActivity.java
@@ -1,52 +1,51 @@
 package com.firstapp.go2meet_map;
 
 import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.os.Build;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
+import android.os.Bundle;
 
-import com.google.android.gms.location.CurrentLocationRequest;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import android.location.LocationManager;
-import android.location.LocationRequest;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.content.Context;
-
-
-import com.firstapp.go2meet_map.databinding.ActivityMapsBinding;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.Circle;
-import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnFailureListener;
+import com.firstapp.go2meet_map.databinding.ActivityMapsBinding;
+import androidx.annotation.RequiresApi;
+
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Build;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import com.google.android.gms.location.CurrentLocationRequest;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.tasks.OnSuccessListener;
+import android.location.LocationRequest;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Context;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -54,14 +53,16 @@ import java.util.List;
 
 
 public class MapsActivity extends FragmentActivity
-        implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+        implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private UiSettings uiSettings;
 
     private ActivityMapsBinding binding;
-    private RadioGroup radioGroup = findViewById(R.id.radioGroup);
-    private RadioButton normalMap = findViewById(R.id.normal_map);
-    private RadioButton hybridMap = findViewById(R.id.hybrid_map);
+    private RadioGroup radioGroup;
+    private RadioButton normalMap;
+
+    private RadioButton hybridMap;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private FusedLocationProviderClient fusedLocationProviderClient ;
      private LocationCallback locationCallback;
@@ -70,7 +71,7 @@ public class MapsActivity extends FragmentActivity
      double longitude;
      double latitude;
 
-     Button btnList;//= findViewById(R.id.homeBtn);
+     //Button btnList;//= findViewById(R.id.homeBtn);
     private static final float DEFAULT_ZOOM = 15f;
 
     //widgets
@@ -80,37 +81,36 @@ public class MapsActivity extends FragmentActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        searchText = (EditText) findViewById(R.id.inputSearch);
+        //searchText = (EditText) findViewById(R.id.inputSearch);
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        radioGroup = findViewById(R.id.radioGroup);
+        normalMap = findViewById(R.id.normal_map);
+        hybridMap = findViewById(R.id.hybrid_map);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(RadioGroup group,int checkedId){
+                if (checkedId == normalMap.getId()) {
+                    mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
-
-        if(checkLocationPermission()){
-            requestCurrentLocation();
-            init();
-        }
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+                } else if (checkedId == hybridMap.getId()) {
+                    mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                }
+            }
+        });
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        mMap.setOnMapClickListener(this);
-
-
-        radioGroup.setOnCheckedChangeListener(
-                new RadioGroup.OnCheckedChangeListener(){
-
-                    public void onCheckedChanged(RadioGroup group,int checkedId){
-                        if (checkedId == normalMap.getId()) {
-                            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-
-                        } else if (checkedId == hybridMap.getId()) {
-                            mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-                        }
-                    }
-                });
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if(checkLocationPermission()){
+            ImageButton currentLocationButton = findViewById(R.id.currentLocationBtn);
+            currentLocationButton.setOnClickListener(view -> {
+                requestCurrentLocation();
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                        new LatLng(latitude, longitude), 15f)
+                );
+            });
+        }
         /*btnList.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(MapsActivity.this, ListActivity.class);
@@ -151,7 +151,7 @@ public class MapsActivity extends FragmentActivity
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
 
-    private void init(){
+    /*private void init(){
         Log.d(TAG,"init: initializing");
         searchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -167,7 +167,7 @@ public class MapsActivity extends FragmentActivity
                 return false;
             }
         });
-    }
+    }*/
     /**   MAKING THE LIST OF SEARCH BAR **/
     /*private void geoLocate(){
         Log.d(TAG,"Geolocate : geolocating");
@@ -197,11 +197,8 @@ public class MapsActivity extends FragmentActivity
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-
-        LatLng sydney = new LatLng(-34, 151); // Add a marker in Sydney and move the camera
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        uiSettings =mMap.getUiSettings();
+        uiSettings.setMyLocationButtonEnabled(false);
         mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         if(checkLocationPermission()){
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
@@ -227,45 +224,15 @@ public class MapsActivity extends FragmentActivity
 
                             }
                         }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-
-                        }
                     });
 
         } catch (SecurityException e){
-
-        }
+            e.printStackTrace();
             }
-
-    private void moveCamera(LatLng latLng, float zoom){
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
     }
     private void setUpMap(GoogleMap googleMap){
 
         // parsed lat and lang ???
 
     }
-    @Override
-    public void onMapClick( LatLng point ) {
-
-        double latitude = point.latitude;
-        double longitude = point.longitude;
-        LatLng centerPoint = new LatLng(latitude, longitude);
-        double radiusMeters = 1000; // 1 kilometer
-        CircleOptions circleOptions = new CircleOptions()
-                .center(centerPoint)
-                .radius(radiusMeters)
-                .strokeWidth(2) // Set the stroke width (border) of the circle
-                .strokeColor(Color.BLUE) // Set the color of the circle's border
-                .fillColor(Color.argb(70, 0, 0, 255)); // Set the color and transparency of the circle's interior
-
-        Circle circle = mMap.addCircle(circleOptions);
-    }
-
-
-
-
 }
