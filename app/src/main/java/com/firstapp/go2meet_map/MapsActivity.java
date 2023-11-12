a49f9744113b325d84aa4d3b8f88b34a76cd2455
kimiyashabani <shabani.kimiya@gmail.com>
2023-11-12 10:26:33 +0100

    getting current location update

--- app/src/main/java/com/firstapp/go2meet_map/MapsActivity.java
+++ app/src/main/java/com/firstapp/go2meet_map/MapsActivity.java
@@ -1,66 +1,65 @@
 package com.firstapp.go2meet_map;
+import androidx.fragment.app.FragmentActivity;
+import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.firstapp.go2meet_map.databinding.ActivityMapsBinding;
 import androidx.annotation.RequiresApi;
+
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Build;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentActivity;
-
 import com.google.android.gms.location.CurrentLocationRequest;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.FusedLocationProviderClient;
-import android.location.LocationManager;
+import com.google.android.gms.tasks.OnSuccessListener;
 import android.location.LocationRequest;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.content.Context;
 
-
-import com.firstapp.go2meet_map.databinding.ActivityMapsBinding;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.Circle;
-import com.google.android.gms.maps.model.CircleOptions;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-
 public class MapsActivity extends FragmentActivity
-        implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+        implements OnMapReadyCallback{
 
     private GoogleMap mMap;
+    private UiSettings uiSettings;
+
 
     private ActivityMapsBinding binding;
-    private RadioGroup radioGroup = findViewById(R.id.radioGroup);
-    private RadioButton normalMap = findViewById(R.id.normal_map);
-    private RadioButton satelliteMap = findViewById(R.id.satellite_map);
-    private RadioButton hybridMap = findViewById(R.id.hybrid_map);
-    private RadioButton terrainMap = findViewById(R.id.terrain_map);
+
+    private RadioGroup radioGroup;
+
+    private RadioButton normalMap;
+
+    private RadioButton hybridMap;
+
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private FusedLocationProviderClient fusedLocationProviderClient ;
      private LocationCallback locationCallback;
@@ -77,42 +76,43 @@ public class MapsActivity extends FragmentActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        searchText = (EditText) findViewById(R.id.inputSearch);
-
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        radioGroup = findViewById(R.id.radioGroup);
+        normalMap = findViewById(R.id.normal_map);
+        hybridMap = findViewById(R.id.hybrid_map);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == normalMap.getId()) {
+                    mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
+                }
+                else if (checkedId == hybridMap.getId()) {
+                    mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+                }
+            }
+        });
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         if(checkLocationPermission()){
-            requestCurrentLocation();
-            init();
+            ImageButton currentLocationButton = findViewById(R.id.currentLocationBtn);
+            currentLocationButton.setOnClickListener(view -> {
+                requestCurrentLocation();
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                        new LatLng(latitude, longitude), 15f)
+                );
+            });
         }
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        /* Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        mMap.setOnMapClickListener(this);
-
-
-        radioGroup.setOnCheckedChangeListener(
-                new RadioGroup.OnCheckedChangeListener(){
-
-                    public void onCheckedChanged(RadioGroup group,int checkedId){
-                        if (checkedId == normalMap.getId()) {
-                            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-
-                        } else if (checkedId == satelliteMap.getId()) {
-                            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-
-                        } else if (checkedId == hybridMap.getId()) {
-                            mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-
-                        } else if (checkedId == terrainMap.getId()) {
-                            mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-                        }
-                    }
-                });
+       mMap.setOnMapClickListener(this);*/
     }
 
     private boolean checkLocationPermission(){
@@ -125,7 +125,7 @@ public class MapsActivity extends FragmentActivity
             return false;
         }
     }
-    //We did not have permission so we requested for it and here we should handle the result of permission REQUEST:
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -138,14 +138,55 @@ public class MapsActivity extends FragmentActivity
                 }
             }else {
                 //permission was denied
-                showToast(this,"he current location cannot be get because there is no permisson to do so\"");
+                showToast(this,"the current location cannot be got because there is no permision to do so");
+
             }
         }
+    }
 
+    public void requestCurrentLocation(){
+        try{
+            CurrentLocationRequest locationRequest = new CurrentLocationRequest.Builder()
+                    .setPriority(LocationRequest.QUALITY_HIGH_ACCURACY)
+                    .setDurationMillis(60000)
+                    .build();
+            //this request location updates
+
+            fusedLocationProviderClient.getCurrentLocation(locationRequest, null)
+                    .addOnSuccessListener(new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }else{
+
+                            }
+                        }
+                    });
+
+        } catch (SecurityException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        uiSettings =mMap.getUiSettings();
+        uiSettings.setMyLocationButtonEnabled(false);
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        if(checkLocationPermission()){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+        }
     }
     private void showToast(Context context, String message) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
+
+
+    /**
 
     private void init(){
         Log.d(TAG,"init: initializing");
@@ -164,6 +205,9 @@ public class MapsActivity extends FragmentActivity
             }
         });
     }
+
+     **/
+
     /**   MAKING THE LIST OF SEARCH BAR **/
     /*private void geoLocate(){
         Log.d(TAG,"Geolocate : geolocating");
@@ -181,85 +225,7 @@ public class MapsActivity extends FragmentActivity
         }
     }*/
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
-
-        LatLng sydney = new LatLng(-34, 151); // Add a marker in Sydney and move the camera
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-        if(checkLocationPermission()){
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
-        }
-    }
-
-    public void requestCurrentLocation(){
-        try{
-            CurrentLocationRequest locationRequest = new CurrentLocationRequest.Builder()
-                    .setPriority(LocationRequest.QUALITY_HIGH_ACCURACY)
-                    .setDurationMillis(60000)
-                    .build();
-            //this request location updates
-            fusedLocationProviderClient.getCurrentLocation(locationRequest, null)
-                    .addOnSuccessListener(new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            if (location != null) {
-                                latitude = location.getLatitude();
-                                longitude = location.getLongitude();
-
-                            }else{
-
-                            }
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-
-                        }
-                    });
-
-        } catch (SecurityException e){
-
-        }
-            }
-
-    private void moveCamera(LatLng latLng, float zoom){
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
-    }
-    private void setUpMap(GoogleMap googleMap){
-
-        // parsed lat and lang ???
-
-    }
-    @Override
-    public void onMapClick( LatLng point ) {
-
-        double latitude = point.latitude;
-        double longitude = point.longitude;
-        LatLng centerPoint = new LatLng(latitude, longitude);
-        double radiusMeters = 1000; // 1 kilometer
-        CircleOptions circleOptions = new CircleOptions()
-                .center(centerPoint)
-                .radius(radiusMeters)
-                .strokeWidth(2) // Set the stroke width (border) of the circle
-                .strokeColor(Color.BLUE) // Set the color of the circle's border
-                .fillColor(Color.argb(70, 0, 0, 255)); // Set the color and transparency of the circle's interior
-
-        Circle circle = mMap.addCircle(circleOptions);
-    }
 
 
 
