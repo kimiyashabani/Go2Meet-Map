5391d9236c0a8ed8e498647a2edfb44a5f58e5cf
kimiyashabani <shabani.kimiya@gmail.com>
2023-11-19 13:40:50 +0100

    light sensor updated and detailedview added

--- app/src/main/java/com/firstapp/go2meet_map/Item.java
+++ app/src/main/java/com/firstapp/go2meet_map/Item.java
@@ -97,4 +97,48 @@ public class Item {
     public String getEventName() {
         return eventName;
     }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public String getWeekdays() {
+        return weekdays;
+    }
+
+    public boolean isFree() {
+        return isFree;
+    }
+
+    public long getLatitude() {
+        return latitude;
+    }
+
+    public long getLongitude() {
+        return longitude;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Long getKey() {
+        return key;
+    }
 }
--- app/src/main/java/com/firstapp/go2meet_map/MapsActivity.java
+++ app/src/main/java/com/firstapp/go2meet_map/MapsActivity.java
@@ -1,11 +1,9 @@
 package com.firstapp.go2meet_map;
 import android.content.res.Resources;
-import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.annotation.RequiresApi;
-import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.FragmentActivity;
 
-import android.graphics.drawable.Drawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -20,42 +18,42 @@ import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.firstapp.go2meet_map.databinding.ActivityMapsBinding;
+
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
 import android.os.Build;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import com.google.android.gms.location.CurrentLocationRequest;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import android.location.LocationRequest;
-import android.widget.TextView;
-import android.widget.Toast;
 import android.content.Context;
-import java.io.IOException;
-import java.util.ArrayList;
+
 import java.util.List;
+
+import android.content.Intent;
+import android.view.View;
+
+
 
 
 public class MapsActivity extends FragmentActivity
         implements OnMapReadyCallback, SensorEventListener {
     /** DEFINING MAP **/
+    Dataset dataset = new Dataset();
     private GoogleMap mMap;
     private UiSettings uiSettings;
     private ActivityMapsBinding binding;
@@ -68,45 +66,43 @@ public class MapsActivity extends FragmentActivity
     private static final String TAG = MapsActivity.class.getSimpleName();
     double longitude;
     double latitude;
+    private GoogleMap googleMap;
 
     /** DEFINING LIGHT SENSOR **/
     private SensorManager sensorManager;
     private Sensor lightSensor;
-    boolean lightSensorActivated;
+    private List<Item> itemsList;
+    Button listButton;
 
-    //Button btnList;//= findViewById(R.id.homeBtn);
-    //widgets
-    private EditText searchText;
+    Marker marker;
+    boolean flag = false;
 
     @RequiresApi(api = Build.VERSION_CODES.S)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //searchText = (EditText) findViewById(R.id.inputSearch);
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        Log.d(TAG, "binding done");
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-
-        if (lightSensor == null) {
-            Toast.makeText(this, "Light sensor not available", Toast.LENGTH_SHORT).show();
-            finish();
+        if (lightSensor != null) {
+            sensorManager.registerListener(lightSensorListener, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
         }
+        listButton = findViewById(R.id.listBtn);
 
         radioGroup = findViewById(R.id.radioGroup);
-        normalMap = findViewById(R.id.normal_map);
         hybridMap = findViewById(R.id.hybrid_map);
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
             @Override
             public void onCheckedChanged(RadioGroup group,int checkedId){
-                if (checkedId == normalMap.getId()) {
-                    mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
-                } else if (checkedId == hybridMap.getId()) {
-                    mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-                }
+                    if (checkedId == hybridMap.getId()) {
+                       AccMap(mMap);
+                    }
+
             }
         });
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
@@ -121,15 +117,28 @@ public class MapsActivity extends FragmentActivity
                 );
             });
         }
-
-
-        //sensorManager.registerListener(MapsActivity.this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        /*btnList.setOnClickListener(new View.OnClickListener() {
+        listButton.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MapsActivity.this, ListActivity.class);
                 startActivity(intent);
             }
-        });*/
+        });
+
+        /*LoadingThread startThread = new LoadingThread(dataset);
+        startThread.start();
+        while (dataset.getListofitems().size() < 80){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }*/
+
+        //itemsList = dataset.getListofitems();
+
+
+
     }
 
     private boolean checkLocationPermission(){
@@ -160,72 +169,34 @@ public class MapsActivity extends FragmentActivity
         }
 
     }
-
-    /*private void init(){
-        Log.d(TAG,"init: initializing");
-        searchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_SEARCH
-                || actionId == EditorInfo.IME_ACTION_DONE
-                || event.getAction() == event.ACTION_DOWN
-                || event.getAction() == event.KEYCODE_ENTER){
-                    //now we execute our method for searching that will find and show the places
-                    //geoLocate();
-
-                }
-                return false;
-            }
-        });
-    }*/
-    /**   MAKING THE LIST OF SEARCH BAR **/
-    /*private void geoLocate(){
-        Log.d(TAG,"Geolocate : geolocating");
-        String searchedString = searchText.getText().toString();
-        Geocoder geocoder = new Geocoder(MapsActivity.this);
-        List<Address> list = new ArrayList<>();
-        try {
-            list = geocoder.getFromLocationName(searchedString , 1);
-        } catch (IOException e){
-            Log.e(TAG, "geolocate : IOException" + e.getMessage());
-        }
-        if (list.size() > 0){
-            Address address = list.get(0);
-            moveCamera(new LatLng(address.getLatitude(), address.getLongitude()), DEFAULT_ZOOM);
-        }
-    }*/
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         uiSettings =mMap.getUiSettings();
         uiSettings.setMyLocationButtonEnabled(false);
-        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         if(checkLocationPermission()){
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
         }
-        try {
-            // Customise the styling of the base map using a JSON object defined
-            // in a raw resource file.
-            boolean success = googleMap.setMapStyle(
-                    MapStyleOptions.loadRawResourceStyle(
-                            this, R.raw.mapstyle_retro));
 
-            if (!success) {
-                Log.e(TAG, "Style parsing failed.");
-            }
-        } catch (Resources.NotFoundException e) {
-            Log.e(TAG, "Can't find style. Error: ", e);
+        /*Adding pins here:
+        for(Item i: itemsList ){
+            LatLng location = new LatLng(i.getLatitude(), i.getLongitude());
+            marker = mMap.addMarker(new MarkerOptions().position(location));
+            marker.setTag(i); // I identified here which marker was clicked
         }
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull LatLng latLng) {
+                int position = (int) marker.getTag();
+                Intent intent = new Intent(MapsActivity.this , DetailedView.class);
+                intent.putExtra("marker position" , position);
+                startActivity(intent);
+            }
+        });*/
+
+
+
     }
 
     public void requestCurrentLocation(){
@@ -242,9 +213,6 @@ public class MapsActivity extends FragmentActivity
                             if (location != null) {
                                 latitude = location.getLatitude();
                                 longitude = location.getLongitude();
-
-                            }else{
-
                             }
                         }
                     });
@@ -253,54 +221,72 @@ public class MapsActivity extends FragmentActivity
             e.printStackTrace();
             }
     }
-    private void setUpMap(GoogleMap googleMap){
 
-        // parsed lat and lang ???
-
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-
-        /*float lightLevel = event.values[0];
-
-        // Adjust this threshold based on your application's requirements
-        if (lightLevel < 1000) {
-            try {
-                boolean success = mMap.setMapStyle(
-                        MapStyleOptions.loadRawResourceStyle(
-                                this, R.raw.mapstyle_night));
-
-                if (!success) {
-                    Log.e(TAG, "Style parsing failed.");
+    SensorEventListener lightSensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float lightLevel = event.values[0];
+            Log.d("TAG", Integer.toString((int) lightLevel));
+            if (lightLevel < 150) {
+                hybridMap.setBackgroundResource(R.drawable.btn_dark_mode);
+                listButton.setBackgroundResource(R.drawable.btn_dark_mode);
+                changeToNightTheme(mMap);
+            } else {
+                hybridMap.setBackgroundResource(R.drawable.btn_round_corner);
+                listButton.setBackgroundResource(R.drawable.btn_round_corner);
+                if (mMap != null) {
+                    applyDefaultMapStyle();
                 }
-            } catch (Resources.NotFoundException e) {
-                Log.e(TAG, "Can't find style. Error: ", e);
             }
         }
 
-         */
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+        }
+    };
+    private void applyDefaultMapStyle() {
+        boolean success = mMap.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(this, R.raw.mapstyle_normal));
+
+        if (!success) {
+            Log.e("TAG", "Default map style parsing failed.");
+        }
     }
+    private void showToast(String message) {Toast.makeText(this, message, Toast.LENGTH_SHORT).show();}
+    @Override
+    public void onSensorChanged(SensorEvent event) {}
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        if (accuracy < 100) {
-            try {
-                boolean success = mMap.setMapStyle(
+
+    }
+
+    public void changeToNightTheme(GoogleMap googleMap){
+        try {
+                boolean success = googleMap.setMapStyle(
                         MapStyleOptions.loadRawResourceStyle(
                                 this, R.raw.mapstyle_night));
-
                 if (!success) {
                     Log.e(TAG, "Style parsing failed.");
                 }
-            } catch (Resources.NotFoundException e) {
-                Log.e(TAG, "Can't find style. Error: ", e);
-            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
         }
     }
+    public void AccMap(GoogleMap googleMap){
+        try {
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.mapstyle_accessible));
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
 
-    private void showToast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
+
+
 }
