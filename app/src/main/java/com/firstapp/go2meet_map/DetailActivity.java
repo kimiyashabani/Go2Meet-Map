e6ac36a9ae928d3f7a2b11166369fc69d4dcd7c7
sofia <sofia.vinan@tum.de>
2023-11-21 21:04:10 +0100

    added functionalities to buttons and link in DetailActivity

--- app/src/main/java/com/firstapp/go2meet_map/DetailActivity.java
+++ app/src/main/java/com/firstapp/go2meet_map/DetailActivity.java
@@ -1,45 +1,33 @@
 package com.firstapp.go2meet_map;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.net.Uri;
-import android.text.method.LinkMovementMethod;
-import android.speech.tts.TextToSpeech;
-import android.widget.Toast;
-import org.w3c.dom.Text;
-
-import java.util.Locale;
 
 public class DetailActivity extends AppCompatActivity {
-    private TextToSpeech textToSpeech;
-    Button cheackMap;
+    Button checkMap;
     Button checkChat;
-    String eventLink;
-    Intent inputIntent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_detail);
-        inputIntent=getIntent();
 
         TextView visitWebsite = findViewById(R.id.linkTextView);
         TextView startTime = findViewById(R.id.startTimeTextView);
         TextView startDate = findViewById(R.id.startDateTextView);
         TextView locationDetails = findViewById(R.id.locationDetailsTextView);
         TextView locationName = findViewById(R.id.locationNameTextView);
-
         checkChat = findViewById(R.id.subscribeButton);
-        cheackMap = findViewById(R.id.backToMapButton);
+        checkMap = findViewById(R.id.backToMapButton);
 
-        eventLink = inputIntent.getStringExtra("link_key");
+
         //Intents to populate fields
         Intent intent = getIntent();
         if (intent == null) {
@@ -58,55 +46,36 @@ public class DetailActivity extends AppCompatActivity {
         locationDetails.setText(intent.getStringExtra("eventLocation"));
         locationName.setText(intent.getStringExtra("eventName"));
 
-        visitWebsite.setMovementMethod(LinkMovementMethod.getInstance());
+
         visitWebsite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse((String)eventLink));
-                startActivity(intent);
-            }
-        });
-
-        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if (status == TextToSpeech.SUCCESS){
-                    int result = textToSpeech.setLanguage(Locale.CANADA);
-                    if (result == TextToSpeech.LANG_MISSING_DATA ||
-                            result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                        Toast.makeText(DetailActivity.this, "Language not supported", Toast.LENGTH_SHORT).show();
-                    }
-                }else {
-                    Toast.makeText(DetailActivity.this, "Initialization failed", Toast.LENGTH_SHORT).show();
+                String url = intent.getStringExtra("link_key");
+                if (url != null) {
+                    Uri webpage = Uri.parse(url);
+                    Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                    startActivity(webIntent);
+                } else {
+                    Log.e("URL", "URL is null");
                 }
             }
         });
 
-        locationName.setOnClickListener(new View.OnClickListener() {
+        checkMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String nameToRead = locationName.getText().toString();
-                textToSpeech.speak(nameToRead, TextToSpeech.QUEUE_FLUSH, null, null);
+                Intent mapIntent = new Intent(DetailActivity.this, MapsActivity.class);
+                startActivity(mapIntent);
             }
         });
 
-
-        cheackMap.setOnClickListener(new View.OnClickListener() {
+        checkChat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(DetailActivity.this, MapsActivity.class);
-                startActivity(intent);
+                Intent chatIntent = new Intent(DetailActivity.this, ChatActivity.class);
+                chatIntent.putExtra("topic", intent.getStringExtra("eventName"));
+                startActivity(chatIntent);
             }
         });
     }
-
-    @Override
-    protected void onDestroy() {
-        if (textToSpeech != null) {
-            textToSpeech.stop();
-            textToSpeech.shutdown();
-        }
-        super.onDestroy();
-    }
-
 }
